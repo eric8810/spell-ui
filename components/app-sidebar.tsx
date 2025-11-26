f6fe80065d1f8ec6cf9f808c2ac4c7ca1c129399
xxtomm <3xteam.dev@gmail.com>
2025-11-26 12:05:09 +0900

    fix: sidebar improvements

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -65,6 +65,7 @@ export function AppSidebar({
             </SidebarGroupContent>
           </SidebarGroup>
         ))}
+        <div className="h-4 shrink-0" />
       </SidebarContent>
     </Sidebar>
   );
