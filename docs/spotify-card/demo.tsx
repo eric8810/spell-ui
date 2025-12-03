2a2db43db6403fac4abf9075f4a97f0d4f6e1402
xxtomm <3xteam.dev@gmail.com>
2025-12-03 18:45:25 +0900

    feat: add spotify card component

---
+++ docs/spotify-card/demo.tsx
@@ -0,0 +1,10 @@
+import { SpotifyCard } from "@/registry/spell-ui/spotify-card";
+
+export default function SpotifyCardDemo() {
+  return (
+    <SpotifyCard
+      url="https://open.spotify.com/track/0uAkm1iQLrzweZ4U1iXy6w"
+      className="max-w-[300px]"
+    />
+  );
+}
