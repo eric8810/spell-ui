cb71705950b563058777d503ed0d247231f6be01
eric8810 <eric8810@eric8810deMac-mini.local>
2026-03-08 12:46:07 +0800

    fix: AppSidebar gradient fade effect for better scroll UX

--- vue/src/components/docs/AppSidebar.vue
+++ vue/src/components/docs/AppSidebar.vue
@@ -30,28 +30,30 @@ const goToDoc = (id: string) => {
 
 <template>
   <Sidebar class="mt-14">
-    <SidebarContent
-      class="max-h-[calc(100vh-100px)] overflow-y-auto"
-      style="mask-image: linear-gradient(to bottom, transparent 0, rgba(0,0,0,0.2) 1rem, black 2rem, black calc(100% - 2rem), rgba(0,0,0,0.2) calc(100% - 1rem), transparent 100%);"
-    >
-      <div class="h-4 shrink-0" />
-      <SidebarGroup v-for="group in props.docSchema" :key="group.title">
-        <SidebarGroupLabel>{{ group.title }}</SidebarGroupLabel>
-        <SidebarGroupContent>
-          <SidebarMenu>
-            <SidebarMenuItem v-for="item in group.items" :key="item.id">
-              <SidebarMenuButton
-                class="data-[active=true]:shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] data-[active=true]:not-dark:bg-white transition-all"
-                :is-active="route.name === 'doc' && route.id === item.id"
-                @click="goToDoc(item.id)"
-              >
-                <span>{{ item.title }}</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroupContent>
-      </SidebarGroup>
-      <div class="h-4 shrink-0" />
-    </SidebarContent>
+    <div class="relative">
+      <div class="pointer-events-none absolute inset-x-0 top-0 z-10 h-8 bg-gradient-to-b from-sidebar to-transparent" />
+      <div class="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-8 bg-gradient-to-t from-sidebar to-transparent" />
+
+      <SidebarContent class="max-h-[calc(100vh-100px)] overflow-y-auto">
+        <div class="h-4 shrink-0" />
+        <SidebarGroup v-for="group in props.docSchema" :key="group.title">
+          <SidebarGroupLabel>{{ group.title }}</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem v-for="item in group.items" :key="item.id">
+                <SidebarMenuButton
+                  class="data-[active=true]:shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] data-[active=true]:not-dark:bg-white transition-all"
+                  :is-active="route.name === 'doc' && route.id === item.id"
+                  @click="goToDoc(item.id)"
+                >
+                  <span>{{ item.title }}</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <div class="h-4 shrink-0" />
+      </SidebarContent>
+    </div>
   </Sidebar>
 </template>
